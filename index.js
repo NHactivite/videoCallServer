1933b969857b32386ca78489dd8d1e0239ce7eba
NIKHIL HAZARIKA <nikhilhazarika9@gmail.com>
2025-04-29 21:26:34 +0530

    fix port 8000

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Server } = require("socket.io");
 const http = require("http");
+const express = require("express");
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors:{
